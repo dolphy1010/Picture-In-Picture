9e682953808300e2ca714770745e54dc0743457f
Joseph <jdolph211@gmail.com>
2020-12-13 08:50:23 -0600

    added Style and Function

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt the user to select a media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (err) {
+        // Catch Errors
+        console.log('Error here', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable the button on Click
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+// onLoad
+selectMediaStream();
